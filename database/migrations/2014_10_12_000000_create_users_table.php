8ad54e6d6120a33854d2c77f3bba8d89ff6c578e
VietDM <saoxa37@gmail.com>
2022-03-23 08:38:25 +0700

    public stub and create template firebase auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('fullname');
+            $table->string('username');
             $table->string('password');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
