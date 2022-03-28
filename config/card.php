c43ffff0ed9b1aefe988d719aa3e947600b81018
AnhDD <anh.dd@zinza.com.vn>
2022-03-28 15:20:57 +0700

    Save

--- config/card.php
+++ config/card.php
@@ -42,5 +42,11 @@ return [
             'image' => 'image/card/garena.jpg',
             'alias' => 'garena'
         ]
+    ],
+    'api' => [
+        'trade' => 'http://api.autocard365.com/api/card',
+        'check-trade' => 'https://api.autocard365.com/api/checktask/{taskid}',
+        'rate' => 'https://api.autocard365.com/api/cardrate?apikey={apikey}',
+        'buy' => 'https://api.autocard365.com/api/buycard'
     ]
 ];
