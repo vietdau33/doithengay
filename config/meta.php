08bcc4553f3fe5e3d3e0293e4efc7a61c5e0466b
VietDM <saoxa37@gmail.com>
2022-04-14 10:57:21 +0700

    hih

--- config/meta.php
+++ config/meta.php
@@ -1,10 +1,19 @@
 <?php
 
+//return [
+//    'title' => 'Hệ thống đổi thẻ cào thành tiền mặt tự động 100% - Mua/ Bán thẻ cào điện thoại, Thẻ game chiết khấu khủng.',
+//    'name' => 'Đổi thẻ cào thành tiền mặt tự động - Thanh toán nhanh - Đổi thẻ ngay!',
+//    'type' => 'Website',
+//    'image' => '/image/logo-trans.png',
+//    'keywork' => 'thẻ điện thoại, thẻ game, thẻ game online, Viettel, Vinaphone, Mobifone, Pubg, zing, Vcoin, Gate, Carot, Garena, Võ lâm, liên quân',
+//    'description' => 'Hệ thống đổi thẻ cào thành tiền mặt tự động 100% - Mua/ Bán thẻ cào điện thoại, Thẻ game chiết khấu khủng.'
+//];
+
 return [
-    'title' => 'Hệ thống đổi thẻ cào thành tiền mặt tự động 100% - Mua/ Bán thẻ cào điện thoại, Thẻ game chiết khấu khủng.',
-    'name' => 'Đổi thẻ cào thành tiền mặt tự động - Thanh toán nhanh - Đổi thẻ ngay!',
+    'title' => 'Hihi',
+    'name' => 'Hihi',
     'type' => 'Website',
     'image' => '/image/logo-trans.png',
-    'keywork' => 'thẻ điện thoại, thẻ game, thẻ game online, Viettel, Vinaphone, Mobifone, Pubg, zing, Vcoin, Gate, Carot, Garena, Võ lâm, liên quân',
-    'description' => 'Hệ thống đổi thẻ cào thành tiền mặt tự động 100% - Mua/ Bán thẻ cào điện thoại, Thẻ game chiết khấu khủng.'
+    'keywork' => 'Hihi',
+    'description' => 'Hihi.'
 ];
