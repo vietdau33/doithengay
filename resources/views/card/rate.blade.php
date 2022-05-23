199f97ea54c00b2b9d334f410bedbca04d4f361e
VietDM <saoxa37@gmail.com>
2022-05-23 23:03:57 +0700

    Fix

--- resources/views/card/rate.blade.php
+++ resources/views/card/rate.blade.php
@@ -5,7 +5,7 @@
             <img src="{{ asset('image/icon/page.svg') }}" alt="Phone" class="mb-2">
             <p class="m-0 font-weight-bold">Chiết khấu đổi thẻ</p>
         </div>
-        <div class="box-content box-pay-card p-3 mt-4">
+        <div class="box-content box-pay-card p-3 mt-4" style="max-width: 750px;">
             @foreach($rates as $name => $rate)
                 <div class="form-header">
                     <img src="{{ asset('image/pay.png') }}" alt="Pay">
@@ -17,7 +17,12 @@
                             <tr>
                                 <th scope="col">#</th>
                                 <th scope="col">Mệnh giá</th>
-                                <th scope="col">Chiết khấu</th>
+                                @if(user()->type_user == 'nomal')
+                                    <th scope="col">Chiết khấu gạch nhanh</th>
+                                    <th scope="col">Chiết khấu gạch chậm</th>
+                                @else
+                                    <th scope="col">Chiết khấu</th>
+                                @endif
                                 <th scope="col">Cập nhật</th>
                             </tr>
                         </thead>
@@ -27,7 +32,14 @@
                                 <tr>
                                     <th scope="row">{{ $stt++ }}</th>
                                     <td>{{ number_format($r['price']) }}</td>
-                                    <td>{{ $r['rate_use'] }}%</td>
+                                    @if(user()->type_user == 'nomal')
+                                        <td>{{ $r['rate_use'] }}%</td>
+                                        <td>{{ $r['rate_slow'] }}%</td>
+                                    @elseif(user()->type_user == 'daily')
+                                        <td>{{ $r['rate_daily'] }}%</td>
+                                    @elseif(user()->type_user == 'tongdaily')
+                                        <td>{{ $r['rate_tongdaily'] }}%</td>
+                                    @endif
                                     <td>{{ date('H:i d/m/Y', strtotime($r['updated_at'])) }}</td>
                                 </tr>
                             @endforeach
--- resources/views/card/trade.blade.php
+++ resources/views/card/trade.blade.php
@@ -1,4 +1,5 @@
 @extends('layout')
 @section('contents')
     @include('template.trade_box')
+    @include('template.table_fee')
 @endsection
